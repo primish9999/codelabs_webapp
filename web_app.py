03caa342dde4a7d64f74a0d9f81d1d12155619e0
Lalla Sankara <47159210+Lalla22@users.noreply.github.com>
2020-08-03 21:53:18 -0400

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -9,41 +9,42 @@ import joblib
 from flask import Flask, request, render_template,jsonify
 from tensorflow import keras
 import numpy as np
+from keras.preprocessing.image import img_to_array, load_img
 # import app 
-
 application = Flask(__name__)
-
-@application.route('/', methods=["GET"])
+global_file_name=""
+@application.route('/', methods=["GET","POST"])
 def index():
+    print()
     return render_template('index.html')
-
 @application.route("/health", methods=["GET"])
 def health():
     return jsonify({"Message": "Service OK"}), 200
-
-@app.route("/upload")
-def upload_file():
-   return render_template('index.html')
-
-@application.route("/upload", methods=["POST"])
+@application.route("/upload", methods=["GET","POST"])
 def upload():
     f = request.files['image']
-    filename="  img-" + str(uuid.uuid4())
+    filename="img-" + str(uuid.uuid4())
     f.save(secure_filename(filename))
-    return jsonify({"Message":"Image Uploaded","FileName":filename}),200
-
+    return render_template('index.html',image=filename)
+    # return jsonify({"Message":"Image Uploaded","FileName":filename}),200
 @application.route("/predict", methods=["POST"])
 def predict():
-    data_arr = [request.form["temperature"],request.form["pO2_saturation"],request.form["leukocyte_count"],request.form["neutrophil_count"],request.form["lymphocyte_count"]]
-    print(data_arr)
-    result,score,status = load_and_predict(data_arr)
+    data = {
+    "temperature":request.form["temperature"],
+    "pO2_saturation":request.form["pO2_saturation"],
+    "leukocyte_count":request.form["leukocyte_count"],
+    "neutrophil_count": request.form["neutrophil_count"],
+    "lymphocyte_count": request.form["lymphocyte_count"],
+    "file_name":request.form["file_name"]
+    }
+    print(data)
+    result,score,status = load_and_predict(data)
     return jsonify({"Status":status, "Prediction":result,"Confidence":score}),200
-
 def load_and_predict(data):
     #This function is not tested
     try:
         tmp = data["temperature"]
-        p02 = data["pO2_saturation"]
+        po2 = data["pO2_saturation"]
         leu_cnt = data["leukocyte_count"]
         neu_cnt = data["neutrophil_count"]
         lym_cnt = data["lymphocyte_count"]
@@ -52,14 +53,15 @@ def load_and_predict(data):
         tf_model_name="chest_xrayimage_covidmodel_4.h5"   #  add filename
         rf_model = joblib.load(rf_model_name)
         trnsfr_learning_model = keras.models.load_model(tf_model_name)
-        rf_probs = rf_model.predict_proba([tmp,po2,leu_cnt,neu_cnt,lym_cnt])
+        rf_probs = rf_model.predict(np.array([tmp,po2,leu_cnt,neu_cnt,lym_cnt]).reshape(1,-1))
         IMG_DIM = (300, 300)
-        img_name = img_to_array(load_img(img_name, IMG_DIM))
-        img_name = np.array(img_name)
-        img_name = img_name.astype('float32')
-        img_name /= 255
-        tf_probs = trnsfr_learning_model.predict_proba([img_name]) #might need to pre-process it maybe
-        rf_probs*tf_probs
+        img = img_to_array(load_img(img_name, target_size = IMG_DIM))
+        img = np.array(img).reshape(None, 300, 300, 3)
+        print('Image Shape: ',img.shape)
+        img = img.astype('float32')
+        img /= 255
+        tf_probs = trnsfr_learning_model.predict([img]) #might need to pre-process it maybe
+        print('Image Shape After: ',img.shape)
         final_probs = [x*y for x,y in zip(rf_probs,tf_probs)]
         label = np.argmax(final_probs)
         if label:
@@ -68,18 +70,17 @@ def load_and_predict(data):
             return "negative",final_probs[label] ,"Success"
     except:
         return None,None,"Error"
-
 if __name__ == '__main__':
     application.run(host='0.0.0.0', port=8889)
-    
 ## Route declaration##
-from flask import current_app as app
-from flask import render_template
-
-
-# @app.route('/')
-# def home():
-#    ##Landing page## 
-#     return render_template('index.html',
-#                            title="ISCovid Site",
-#                            description="Upload X-Ray")
+	# from flask import current_app as app
+	# from flask import render_template
+	# 
+	# 
+	# # @app.route('/')
+	# # def home():
+	# #    ##Landing page## 
+	# #     return render_template('index.html',
+	# #                            title="ISCovid Site",
+	# #                            description="Upload X-Ray")
+	# Collapse
